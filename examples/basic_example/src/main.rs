bb3f2a5cb1ac95b01fa2c19b69d93f0b812614bf
George Manning <gmanning@live.co.uk>
2023-05-06 01:35:12 +0100

    update example

--- examples/basic_example/src/main.rs
+++ examples/basic_example/src/main.rs
@@ -1,19 +1,13 @@
 use weaviate_client::command::meta::MetaResponse;
-use weaviate_client::connection::{Connection, ConnectionParams};
+use weaviate_client::ConnectionBuilder;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let sdk = Connection::new(ConnectionParams {
-        api_key: "".to_string(),
-        host: "localhost:8080".to_string(),
-        scheme: "http".to_string(),
-        headers: None,
-        auth_client_secret: None,
-    });
+    let sdk = ConnectionBuilder::new("http", "localhost:8080").build();
 
     let meta = sdk
-        .http
-        .get("/meta".to_string(), None)
+        .client
+        .get("/meta")
         .await?
         .json::<MetaResponse>()
         .await?;
